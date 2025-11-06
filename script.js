2f8e1da330e4fe9b439561c36210883894f0de9f
Kanhaiya Arora <kanhaiyaarora75@gmail.com>
2025-11-07 00:29:06 +0530

    mini project 2

--- script.js
+++ script.js
@@ -90,5 +90,8 @@ btn.addEventListener("click", function () {
 });
 
 inpfile.addEventListener("change", function (dets) {
-  btn.textContent = dets.target.files[0].name;
+  const file = dets.target.files[0];
+  if (file) {
+    btn.textContent = file.name;
+  }
 });
